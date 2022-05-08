fb54548e832ff5264a65d0e84b66a6a2c7666c83
Patrick Marshall <marshallpt@vcu.edu>
2022-05-08 01:31:19 -0400

    Added parse_file_list function
    Added function to sort file list by time modified, so final result
    will be in chronological order. This function now also handles
    the pattern matching for determining which files to add to the
    AviSynth script via the extensions.
    
    avisynth_writer.py
    * added parse_file_list function
    * added sort_by_modified_date function

--- dvd-creation/avisynth_writer.py
+++ dvd-creation/avisynth_writer.py
@@ -1,4 +1,31 @@
 import os
+import re
+
+
+def sort_by_modified_date(entry):
+    """Return time file was modified."""
+    return os.path.getmtime(entry)
+
+
+def parse_file_list(file_list, extensions):
+    """
+    Sort file list by date modified and return list only containing passed extensions.
+
+    :param file_list: list of files in the cwd
+    :param extensions: list of desired file extensions in new file_list
+    :return: sorted & pruned file_list
+    """
+    new_list = []
+    file_list.sort(key=sort_by_modified_date)
+
+    for entry in file_list:
+        for x in extensions:
+            regex_compare = f"\.{x}$"
+            match = re.search(regex_compare, entry.name)
+            if match is not None:
+                new_list.append(entry)
+
+    return new_list
 
 
 def generate_avs_file(proj_name, file_list):
@@ -9,25 +36,17 @@ def generate_avs_file(proj_name, file_list):
     :param file_list: DirEntry list : list of files to add to the AviSynth file
     :return: no return value
     """
-    extension = '.mpg'
-    ignored_extension = '.ffindex'
-    avs_name = proj_name + '.avs'
-
-    avs_file = open(avs_name, "w+")
-
+    avs_file = open(f"{proj_name}.avs", "w+")
     count = 0
 
     for entry in file_list:
-        if entry.name.find(extension) != -1 and entry.name.find(ignored_extension) == -1:
-            print(entry, end='')
-            print(os.path.getmtime(entry))
-            if count != 0:
-                avs_file.write('  ++ \\\n')
-            avs_line = f'FFmpegSource2("{entry.path}", atrack = -1)'
-            avs_file.write(avs_line)
-            count += 1
+        print(f"{entry}, {os.path.getmtime(entry)}")
+        if count != 0:
+            avs_file.write('  ++ \\\n')
+        avs_line = f'FFmpegSource2("{entry.path}", atrack = -1)'
+        avs_file.write(avs_line)
+        count += 1
     avs_file.close()
-    file_list.close()
 
 
 def generate_bat_file(proj_name):
@@ -45,16 +64,19 @@ def generate_bat_file(proj_name):
 
 
 def main():
-    """Use the current working directory to generate a project name and file list, and then generate
-    a .avs and .bat file."""
-    working_directory = os.getcwd()
-    split_path = working_directory.split('\\')
-    file_list = os.scandir(working_directory)
-    print(f"Operating path: {working_directory}\nFiles found: ")
+    """Generate an AviSynth script to concatenate all video files in cwd, as well as a .bat file
+    to encode it for DVD via ffmpeg."""
+    cwd = os.getcwd()
+    split_path = cwd.split('\\')
+    file_list = list(os.scandir(cwd))
 
-    project_name = split_path[-1]
-    generate_avs_file(proj_name=project_name, file_list=file_list)
-    generate_bat_file(proj_name=project_name)
+    extensions = ['MOD', 'mpg']
+    proj_name = split_path[-1]
+    file_list = parse_file_list(file_list=file_list, extensions=extensions)
+
+    print(f"Operating path: {cwd}\nFiles found: ")
+    generate_avs_file(proj_name=proj_name, file_list=file_list)
+    generate_bat_file(proj_name=proj_name)
 
 
 if __name__ == '__main__':
