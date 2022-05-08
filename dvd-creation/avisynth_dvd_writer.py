f483fceca682ad9600116fd6d765c40c02b90a66
Patrick Marshall <marshallpt@vcu.edu>
2022-05-08 09:11:02 -0400

    Improved PEP-8 Compliance
    Now receives a 10/10 on pylint.
    
    avisynth_dvd_writer.py
    * made file name more descriptive
    * added module docstring
    * renamed sorting key function
    * updated RegEx expression to be denoted as such
    * wrapped file openings in with statements
    * reorganized main to make more sense

--- dvd-creation/avisynth_dvd_writer.py
+++ dvd-creation/avisynth_dvd_writer.py
@@ -1,8 +1,18 @@
+"""AviSynth DVD Writer
+
+This script concatenates every video in its running directory in a single .avs file and generates
+a .bat file to encode it via FFMPEG for DVD delivery.
+
+    * modified_date - returns date file was modified
+    * parse_file_list - sorts & filters file list
+    * generate_avs_file - concatenates all passed files into one .avs file
+    * generate_bat_file - generates .bat file to ffmpeg encode .avs file
+"""
 import os
 import re
 
 
-def sort_by_modified_date(entry):
+def modified_date(entry):
     """Return time file was modified."""
     return os.path.getmtime(entry)
 
@@ -16,11 +26,11 @@ def parse_file_list(file_list, extensions):
     :return: sorted & pruned file_list
     """
     new_list = []
-    file_list.sort(key=sort_by_modified_date)
+    file_list.sort(key=modified_date)
 
     for entry in file_list:
-        for x in extensions:
-            regex_compare = f"\.{x}$"
+        for ext in extensions:
+            regex_compare = rf"\.{ext}$"
             match = re.search(regex_compare, entry.name)
             if match is not None:
                 new_list.append(entry)
@@ -36,16 +46,16 @@ def generate_avs_file(proj_name, file_list):
     :param file_list: DirEntry list : list of files to add to the AviSynth file
     :return: no return value
     """
-    avs_file = open(f"{proj_name}.avs", "w+")
-    count = 0
+    with open(f"{proj_name}.avs", mode="w", encoding="UTF-8") as avs_file:
+        count = 0
 
-    for entry in file_list:
-        print(f"{entry}, {os.path.getmtime(entry)}")
-        if count != 0:
-            avs_file.write('  ++ \\\n')
-        avs_line = f'FFmpegSource2("{entry.path}", atrack = -1)'
-        avs_file.write(avs_line)
-        count += 1
+        for entry in file_list:
+            print(f"{entry}, {os.path.getmtime(entry)}")
+            if count != 0:
+                avs_file.write('  ++ \\\n')
+            avs_line = f'FFmpegSource2("{entry.path}", atrack = -1)'
+            avs_file.write(avs_line)
+            count += 1
     avs_file.close()
 
 
@@ -56,10 +66,10 @@ def generate_bat_file(proj_name):
     :param proj_name: string : name of .avs file to point new .bat file to
     :return: no return value
     """
-    batch_file = open(f"{proj_name}.bat", "w+")
-    batch_line = f'ffmpeg -i {proj_name}.avs -target ntsc-dvd -r 29.97 -s 720x480 -aspect 16:9 -b:v 8000k ' \
-                 f'{proj_name}.mpeg \npause'
-    batch_file.write(batch_line)
+    with open(f"{proj_name}.bat", mode="w", encoding="UTF-8") as batch_file:
+        batch_line = f'ffmpeg -i {proj_name}.avs -target ntsc-dvd -r 29.97 -s 720x480 ' \
+                     f'-aspect 16:9 -b:v 8000k {proj_name}.mpeg \npause'
+        batch_file.write(batch_line)
     batch_file.close()
 
 
@@ -68,10 +78,10 @@ def main():
     to encode it for DVD via ffmpeg."""
     cwd = os.getcwd()
     split_path = cwd.split('\\')
+    proj_name = split_path[-1]
     file_list = list(os.scandir(cwd))
 
     extensions = ['MOD', 'mpg']
-    proj_name = split_path[-1]
     file_list = parse_file_list(file_list=file_list, extensions=extensions)
 
     print(f"Operating path: {cwd}\nFiles found: ")
